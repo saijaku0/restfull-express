bff1ced3ba522d6273256cf026f99b667789e70d
Vadym <minerking112256@gmail.com>
2026-09-14 21:29:37 +0200

    Update user.controller.ts

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { getProfile, updateProfile } from './user.service';
+import { getProfile, updateProfile, changePassword, listUsers } from './user.service';
 
 export async function getMyProfile(req: Request, res: Response, next: NextFunction) {
   try {
@@ -14,6 +14,24 @@ export async function updateMyProfile(req: Request, res: Response, next: NextFun
   try {
     const user = await updateProfile(req.user!.userId, req.body);
     return res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function changeMyPassword(req: Request, res: Response, next: NextFunction) {
+  try {
+    await changePassword(req.user!.userId, req.body.oldPassword, req.body.newPassword);
+    return res.status(200).json({ message: 'Password changed successfully' });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function getAllUsers(req: Request, res: Response, next: NextFunction) {
+  try {
+    const result = await listUsers(req.query as any);
+    return res.status(200).json(result);
   } catch (err) {
     next(err);
   }
