3d9b21081db35fe523c3edb47ec777950354921b
Vadym <minerking112256@gmail.com>
2026-09-14 21:29:29 +0200

    Update user.schema.ts

--- src/modules/users/user.schema.ts
+++ src/modules/users/user.schema.ts
@@ -5,3 +5,13 @@ export const updateProfileSchema = Joi.object({
   lastName: Joi.string().min(1),
   email: Joi.string().email(),
 }).min(1);
+
+export const changePasswordSchema = Joi.object({
+  oldPassword: Joi.string().required(),
+  newPassword: Joi.string().min(6).required(),
+});
+
+export const listUsersSchema = Joi.object({
+  page: Joi.number().integer().min(1).default(1),
+  limit: Joi.number().integer().min(1).max(100).default(10),
+});
